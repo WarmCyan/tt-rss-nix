265f125858cd8cb44a4a6ad4de007c71aa899bb3
Martindale, Nathan <nathanamartindale@gmail.com>
2025-12-11 20:58:10 -0500

    Update freshapi

--- pkgs/tt-rss-plugin-freshapi.nix
+++ pkgs/tt-rss-plugin-freshapi.nix
@@ -11,26 +11,50 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "eric-pierce";
     repo = "freshapi";
-    rev = "44c98f12e8a4423501fc6d8cb7903cca11094dc6";
-    hash = "sha256-1cQ4QMrXOdtelAbmMEuhWJPFi5XrAoR3IGlFzb8122k=";
+    rev = "942b1c37ef2035444c3a22a21e225f3ece73c705"; # this and the line below needs to be updated based on the git sha of freshapi you want to install
+    sha256 = "sha256-XWzEya+1A/UtwTL+HseoX8trEypNjHurK/VF3k2seMQ";
   };
 
   installPhase = ''
-    runHook preInstall
-
-    install -D init.php $out/freshapi/init.php
-    install -D api/greader.php $out/freshapi/api/greader.php
-    install -D api/freshapi.php $out/freshapi/api/freshapi.php
-
-    runHook postInstall
+    mkdir -p $out/freshapi
+    cp -r api init.php $out/freshapi
   '';
 
-  meta = {
-    description = "FreshRSS / Google Reader API Plugin for Tiny-Tiny RSS";
-    license = lib.licenses.agpl3Only;
+  meta = with lib; {
+    description = "Tiny Tiny RSS FreshAPI Plugin";
+    longDescription = ''
+      A FreshRSS / Google Reader API Plugin for Tiny-Tiny RSS
+    '';
+    license = with licenses; [ agpl3Only ];
     homepage = "https://github.com/eric-pierce/freshapi";
-    maintainers = with lib.maintainers; [ wrvsrx ];
-    platforms = lib.platforms.all;
+    maintainers = with maintainers; [ bigloser ];
+    inherit (tt-rss.meta) platforms;
   };
+
+
+  # src = fetchFromGitHub {
+  #   owner = "eric-pierce";
+  #   repo = "freshapi";
+  #   rev = "44c98f12e8a4423501fc6d8cb7903cca11094dc6";
+  #   hash = "sha256-1cQ4QMrXOdtelAbmMEuhWJPFi5XrAoR3IGlFzb8122k=";
+  # };
+  #
+  # installPhase = ''
+  #   runHook preInstall
+  #
+  #   install -D init.php $out/freshapi/init.php
+  #   install -D api/greader.php $out/freshapi/api/greader.php
+  #   install -D api/freshapi.php $out/freshapi/api/freshapi.php
+  #
+  #   runHook postInstall
+  # '';
+  #
+  # meta = {
+  #   description = "FreshRSS / Google Reader API Plugin for Tiny-Tiny RSS";
+  #   license = lib.licenses.agpl3Only;
+  #   homepage = "https://github.com/eric-pierce/freshapi";
+  #   maintainers = with lib.maintainers; [ wrvsrx ];
+  #   platforms = lib.platforms.all;
+  # };
 }
 
